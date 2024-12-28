b155ba3b29e520aeda70890ec4ef941c2ceec190
Wadhudh Kavi <wadhudhkavi520@gmail.com>
2024-12-28 12:17:13 +0530

    Greetings Program Practiced

---
+++ src/Base/Lec11_Methods/Greetings.java
@@ -0,0 +1,11 @@
+package Base.Lec11_Methods;
+
+public class Greetings {
+    public static void main(String[] args) {
+        greet();
+    }
+    static void greet()
+    {
+        System.out.println("Hello World");
+    }
+}
