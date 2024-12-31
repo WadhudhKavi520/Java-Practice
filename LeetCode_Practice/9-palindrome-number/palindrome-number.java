b37d6d2805922166438f90f44bd53e64cd1035b0
WadhudhKavi520 <118262528+WadhudhKavi520@users.noreply.github.com>
2024-12-31 23:23:51 +0530

    Time: 5 ms (71.60%) | Memory: 44.2 MB (40.60%) - LeetSync

--- LeetCode_Practice/9-palindrome-number/palindrome-number.java
+++ LeetCode_Practice/9-palindrome-number/palindrome-number.java
@@ -1,26 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int original = x;        
-        //String revNumber = "";//revNumber stands for reversed number.
-        
-        // if(x==0) {
-        //     return true;
-        // }
-        if(x<0) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
             return false;
         }
 
-        int revNumber = x%10;
-        x = x/10;
-        
-        while(x>0) {
-            //revNumber = revNumber + Integer.toString(x%10);
-            revNumber = revNumber*10 + (x%10);
-            x = x/10;            
-        }        
+        int reversed = 0;
+        int original = x;
 
-        System.out.println(revNumber);
-        //return revNumber.equals(Integer.toString(original));
-        return revNumber==original;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == reversed) || (x == reversed / 10);
     }
 }
