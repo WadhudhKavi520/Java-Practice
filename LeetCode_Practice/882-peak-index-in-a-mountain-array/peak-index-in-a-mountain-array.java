048054a4738ad82d7f7b14044e886a6803a3e50a
WadhudhKavi520 <118262528+WadhudhKavi520@users.noreply.github.com>
2025-01-12 11:35:16 +0530

    Time: 0 ms (100.00%) | Memory: 56.5 MB (31.86%) - LeetSync

--- LeetCode_Practice/882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ LeetCode_Practice/882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -5,14 +5,13 @@ class Solution {
 
         while(start<=end) {
             int mid = start + ( (end-start)/2 );
-            //System.out.println(start + " " + end + " " + mid + " ");
             if(mid==0) {
                 return mid+1;
             }
             else if(mid==arr.length-1) {
                 return mid-1;
             }
-            if( (arr[mid] > arr[mid-1]) && (arr[mid] > arr[mid+1]) ){//peak reached
+            else if( (arr[mid] > arr[mid-1]) && (arr[mid] > arr[mid+1]) ){//peak reached
                 return mid;
             }
             else if( (arr[mid] > arr[mid-1]) && (arr[mid] < arr[mid+1]) ){//growing phase
