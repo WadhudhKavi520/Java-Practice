fc83d83505c007b5444cb58f035fb812010ac56a
Wadhudh Kavi <wadhudhkavi520@gmail.com>
2025-01-13 18:35:52 +0530

    Bubble sort optimized by considering no swap conditions amidst execution

--- src/Base/Lec17_Bubble_Sort/BubbleSort.java
+++ src/Base/Lec17_Bubble_Sort/BubbleSort.java
@@ -5,13 +5,19 @@ import java.util.Arrays;
 public class BubbleSort {
     static void bubbleSort(int[] arr) {
         for(int i=0; i<arr.length; i++) {
+            boolean swapped = false;
             for(int j=1; j<arr.length-i; j++) {
                 if(arr[j] < arr[j-1]) {
                     //swap
                     int temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
+                    swapped = true;
                 }
+            }
+            if(!swapped) {//bubble sort algorithm terminates if no swapping occurs for any value of j,
+                            //because it means the array has already been completely sorted.
+                break;
             }
         }
     }
