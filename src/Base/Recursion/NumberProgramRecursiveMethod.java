0d06a41773e9a9df985669ebbb0764c54377b9d6
Wadhudh Kavi <wadhudhkavi520@gmail.com>
2025-08-29 12:29:05 +0530

    Recursion Basic Concepts Practiced

---
+++ src/Base/Recursion/NumberProgramRecursiveMethod.java
@@ -0,0 +1,17 @@
+package Base.Recursion;
+
+public class NumberProgramRecursiveMethod {
+    public static void main(String[] args) {
+        int n = 1;
+        print(n);
+    }
+
+    static void print(int n) {
+        if(n==5) {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    }
+}
